ac01c8da30b5c9243ee752d3e939d85d12c9c641
ALDEN <trungmai12121970@gmail.com>
2026-01-23 15:50:08 +0700

    thêm bài tuần 2 và example

---
+++ src/Example.java
@@ -0,0 +1,32 @@
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Example {
+    public static void main(String[] args){
+        Ex1();
+        Ex2();
+    }
+    public static void Ex1(){
+        try{
+            String domain="sgu.edu.vn";
+            InetAddress inet=InetAddress.getByName(domain);
+            System.out.println("Domain "+domain+" có IP "+inet.getHostAddress());
+
+        }catch (UnknownHostException e){
+            System.err.println(e.getMessage());
+        }
+    }
+    public static void Ex2(){
+        try{
+            String domain1="amazon.com";
+            InetAddress[] inetAddList=InetAddress.getAllByName(domain1);
+            for(InetAddress inetAdd: inetAddList){
+                System.out.println(inetAdd.getHostAddress());
+            }
+
+        }catch (UnknownHostException e){
+            System.err.println(e.getMessage());
+        }
+    }
+}
--- src/Tuan2.java
+++ src/Tuan2.java
@@ -1,8 +1,46 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Tuan2 {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        String input=scanner.nextLine();
+        double result=0;
+        StringTokenizer st=new StringTokenizer(input,"+-*/",true);
+        if(st.hasMoreTokens()){
+            result=Double.parseDouble(st.nextToken().trim());
+        }
+        while (st.hasMoreTokens()){
+            String operation=st.nextToken().trim();
+            if(st.hasMoreTokens()){
+                double nextnum=Double.parseDouble(st.nextToken().trim());
+                switch (operation){
+                    case "+": result+=nextnum;
+                        break;
+                    case "-": result-=nextnum;
+                        break;
+                    case "*": result*=nextnum;
+                        break;
+                    case "/": result/=nextnum;
+                        break;
+            }
 
 
 
-}
+
+
+                System.out.println(result);
+        }
+
+
+
+
+
+
+    }
+
+
+}}
 
 
 
