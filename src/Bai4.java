75505e872571256943a46ff9191b38df7db40542
ALDEN <trungmai12121970@gmail.com>
2026-01-29 22:16:04 +0700

    thêm cái nhâp exit

--- src/Bai4.java
+++ src/Bai4.java
@@ -7,41 +7,58 @@ import java.util.StringTokenizer;
 
 public class Bai4 {
     public static void main(String[] args){
-        Scanner scanner=new Scanner(System.in);
-        long n=scanner.nextLong();
+
         LinkedHashMap<Long,Integer> ATM=new LinkedHashMap<>();
         LinkedHashMap<Long,Integer> result=new LinkedHashMap<>();
 
         File ATMFile=new File("src/ATM.txt");
+        while(true){
+            System.out.println("\nNhập số tiền muốn rút hoặc exit để thoát ");
+            Scanner console=new Scanner(System.in);
+            String command=console.nextLine();
 
-        try(Scanner reader=new Scanner(ATMFile)){
-            while (reader.hasNextLine()){
-                String data=reader.nextLine();
-                StringTokenizer st=new StringTokenizer(data,";");
-                long price= Long.parseLong(st.nextToken());
-                int quantity= Integer.parseInt(st.nextToken());
-                ATM.putIfAbsent(price,quantity);
+            if(command.equalsIgnoreCase("exit")) {
+                break;
             }
-            for(long key: ATM.keySet()){
-                while(n>=key&&ATM.get(key)>0){
-                    n-=key;
-                    ATM.put(key,ATM.get(key)-1);
-                    result.put(key,result.getOrDefault(key,0)+1);
-
+            System.out.println("Nhập số tiền muốn rút");
+            Scanner scanner=new Scanner(System.in);
+            long n=scanner.nextLong();
+            try(Scanner reader=new Scanner(ATMFile)){
+                while (reader.hasNextLine()){
+                    String data=reader.nextLine();
+                    StringTokenizer st=new StringTokenizer(data,";");
+                    long price= Long.parseLong(st.nextToken());
+                    int quantity= Integer.parseInt(st.nextToken());
+                    ATM.putIfAbsent(price,quantity);
                 }
-            }
-            if(n==0){
-                System.out.print("Các menh gia duoc rut là ");
-                for(long key: result.keySet()){
-                    System.out.print(key+" * "+result.get(key));
+                for(long key: ATM.keySet()){
+                    while(n>=key&&ATM.get(key)>0){
+                        n-=key;
+                        ATM.put(key,ATM.get(key)-1);
+                        result.put(key,result.getOrDefault(key,0)+1);
+
+                    }
                 }
-            }else{
-                System.out.println("Không thể rút số tiền này");
+                if(n==0){
+                    System.out.print("Các menh gia duoc rut là ");
+                    for(long key: result.keySet()){
+                        System.out.print(" "+key+" * "+result.get(key));
+                    }
+                }else{
+                    System.out.println("Không thể rút số tiền này");
+                }
+
+            }catch (FileNotFoundException e){
+                System.err.print(e.getMessage());
             }
 
-        }catch (FileNotFoundException e){
-            System.err.print(e.getMessage());
         }
 
+
     }
+
+
+
+
+
 }
